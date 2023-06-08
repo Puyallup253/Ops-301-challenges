f005fa8eb918c6c2d487d331cd1ecc54e356f361
Robert Gillespie <gillespier523@gmail.com>
2023-06-08 19:34:26 +0000

    challenge 8

---
+++ Ops301challenge8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Script Name:                 code challenge 7
+# Author:                       Robert Gillespie
+# Date of latest revision:      6/8/2023
+# Purpose:
+
+# Assign a list of ten string elements to a variable
+my_list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "imbe", "jackfruit"]
+
+# Print the fourth element of the list (index 3)
+print("Fourth element:", my_list[3])
+
+# Print the sixth through tenth element of the list (index 5 to 9)
+print("Sixth to tenth elements:", my_list[5:])
+
+# Change the value of the seventh element (index 6) to "onion"
+my_list[6] = "onion"
+
+# Print the updated list
+print("Updated list:", my_list)
